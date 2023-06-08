d737501a8be90b3952a5df5702ab76d4d0d49409
Ayeniko Emmanuel <ayenikoemmanuel06@gmail.com>
2023-06-08 12:45:44 +0100

    added fonts

--- components/FinishedOrdersPage.tsx
+++ components/FinishedOrdersPage.tsx
@@ -3,8 +3,12 @@ import { View, TouchableOpacity, Text, Image, ScrollView } from "react-native";
 import { SvgXml } from "react-native-svg";
 import { styled } from "nativewind";
 import { backSvg } from "../helpers/svgs";
-
 import OrderItem from "./Order";
+import {
+  useFonts,
+  Montserrat_600SemiBold,
+  Montserrat_400Regular,
+} from "@expo-google-fonts/montserrat";
 
 const StyledView = styled(View);
 const StyledText = styled(Text);
@@ -50,41 +54,70 @@ const ordersData = [
 ];
 
 const FinishedOrdersPage = () => {
+  let [fontsLoaded] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
     <ScrollView>
       <StyledView className="mx-[28]">
         <TouchableOpacity className="px-[18] py-[13] rounded-lg border-[1px] border-gray-200 mt-[10] flex items-center w-[15] bg-[#F2F2F2]">
           <SvgXml xml={backSvg} width={18} height={18} />
         </TouchableOpacity>
-        <StyledText className="text-[30px] mt-[10] mb-[20] font-[600]">
+        <StyledText
+          style={{ fontFamily: "Montserrat_600SemiBold" }}
+          className="text-[30px] mt-[10] mb-[20] font-[600]"
+        >
           Finished orders
         </StyledText>
         <StyledView className="border-[1px] border-[#D9D9D9] flex justify-center pt-[40] pb-[30] rounded-2xl">
           <StyledImage source={i1} className="flex mx-auto" />
           <StyledView className="flex flex-col pt-[20] gap-1 ml-[33]">
-            <StyledText className="text-[18px] tracking-wider font-semibold ml-[33]">
+            <StyledText
+              style={{ fontFamily: "Montserrat_600SemiBold" }}
+              className="text-[18px] tracking-wider font-semibold ml-[33]"
+            >
               Angel R.
             </StyledText>
-            <StyledText className="text-[17px] text-[#00000080]  ml-[33]">
+            <StyledText
+              style={{ fontFamily: "Montserrat_400Regular" }}
+              className="text-[17px] text-[#00000080]  ml-[33]"
+            >
               TV Mounting 2:00 pm
             </StyledText>
             <StyledView className="flex flex-row mx-[33] justify-between">
-              <StyledText className="font-semibold text-[17px] text-[#00000080]">
+              <StyledText
+                style={{ fontFamily: "Montserrat_600SemiBold" }}
+                className="font-semibold text-[17px] text-[#00000080]"
+              >
                 $80.73/hr
               </StyledText>
-              <StyledText className="text-[#00000080] text-[17px]">
+              <StyledText
+                style={{ fontFamily: "Montserrat_400Regular" }}
+                className="text-[#00000080] text-[17px]"
+              >
                 1:14:59
               </StyledText>
             </StyledView>
           </StyledView>
           <StyledView className="flex flex-row gap-2 justify-center mx-[33] pt-[18]">
             <TouchableOpacity className="bg-[#D9D9D9] rounded-xl">
-              <StyledText className=" px-[30] py-[6] text-[17px]">
+              <StyledText
+                style={{ fontFamily: "Montserrat_400Regular" }}
+                className=" px-[30] py-[6] text-[17px]"
+              >
                 Report issue
               </StyledText>
             </TouchableOpacity>
             <TouchableOpacity className="bg-[#58C0DB] rounded-xl">
-              <StyledText className="text-white px-[50] py-[6] text-[17px]">
+              <StyledText
+                style={{ fontFamily: "Montserrat_400Regular" }}
+                className="text-white px-[50] py-[6] text-[17px]"
+              >
                 Done
               </StyledText>
             </TouchableOpacity>
--- components/Order.tsx
+++ components/Order.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import {
   View,
-  TouchableOpacity,
   Text,
   Image,
   ImageSourcePropType,
+  TouchableOpacity,
 } from "react-native";
 import { SvgXml } from "react-native-svg";
 import { styled } from "nativewind";
 import { rescheduleSvg } from "../helpers/svgs";
+import {
+  useFonts,
+  Montserrat_600SemiBold,
+  Montserrat_400Regular,
+  Montserrat_500Medium,
+} from "@expo-google-fonts/montserrat";
 
 const StyledView = styled(View);
 const StyledText = styled(Text);
@@ -23,20 +29,48 @@ interface Orders {
 }
 
 const OrderItem = ({ name, work, item, amount, img }: Orders) => {
+  let [fontsLoaded] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_600SemiBold,
+    Montserrat_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
     <StyledView className="flex flex-row justify-between mt-5 items-center">
       <StyledImage source={img} />
       <StyledView className="">
-        <StyledText className="text-[15px] tracking-wider font-semibold">
+        <StyledText
+          style={{ fontFamily: "Montserrat_600SemiBold" }}
+          className="text-[15px] tracking-wider font-semibold"
+        >
           {name}
         </StyledText>
-        <StyledText className="text-[15px] tracking-wider">{work}</StyledText>
-        <StyledText className="text-[#00000080] text-[15px]">{item}</StyledText>
-        <StyledText className="text-[#00000080] text-[15px]">
+
+        <StyledText
+          style={{ fontFamily: "Montserrat_500Medium" }}
+          className="text-[15px] tracking-wider"
+        >
+          {work}
+        </StyledText>
+        <StyledText
+          style={{ fontFamily: "Montserrat_400Regular" }}
+          className="text-[#00000080] text-[15px]"
+        >
+          {item}
+        </StyledText>
+        <StyledText
+          style={{ fontFamily: "Montserrat_400Regular" }}
+          className="text-[#00000080] text-[15px]"
+        >
           {amount}
         </StyledText>
       </StyledView>
-      <SvgXml xml={rescheduleSvg} width={26} height={26} />
+      <TouchableOpacity>
+        <SvgXml xml={rescheduleSvg} width={26} height={26} />
+      </TouchableOpacity>
     </StyledView>
   );
 };
